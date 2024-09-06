da29266746f42a523474df7dab537d00894c28a3
kimongseong <rlaghdtjd123@gmail.com>
2024-09-06 20:27:21 +0900

    feat: 유레카서버,GW 구현
    - 유레카 서버 생성
    - 게이트웨이 생성

---
+++ eurekserver/src/main/java/com/sparta/hotsix/eurekserver/EurekserverApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hotsix.eurekserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekserverApplication.class, args);
+    }
+
+}
---
+++ eurekserver/src/test/java/com/sparta/hotsix/eurekserver/EurekserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hotsix.eurekserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
