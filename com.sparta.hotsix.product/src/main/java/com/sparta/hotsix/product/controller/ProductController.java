bb50be4c93b2ece0b8abb563a66beee02e4d4962
sanghun <tkdgns0236@gmail.com>
2024-09-08 19:54:01 +0900

    feat: Controller 작성

---
+++ com.sparta.hotsix.product/src/main/java/com/sparta/hotsix/product/controller/ProductController.java
@@ -0,0 +1,59 @@
+package com.sparta.hotsix.product.controller;
+
+import com.sparta.hotsix.product.domain.dto.ProductDto;
+import com.sparta.hotsix.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ProductDto.Response createProduct(@RequestBody ProductDto.Create productDto) {
+        return productService.createProduct(productDto);
+    }
+
+    @GetMapping("/{productId}")
+    public ProductDto.Response getProduct(@PathVariable UUID productId) {
+        return productService.getProduct(productId);
+    }
+
+    @GetMapping
+    public List<ProductDto.GetAllProductsResponse> getAllProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String sortBy
+    ) {
+        return productService.getAllProducts(page, size, sortBy);
+    }
+
+    @GetMapping("/search")
+    public List<ProductDto.GetAllProductsResponse> searchProduct(
+            @RequestParam("name") String name,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sort") String sortBy
+    ) {
+        return productService.searchProduct(name, page, size, sortBy);
+    }
+
+    @PatchMapping("/{productId}")
+    public ProductDto.Response modifyProduct(@PathVariable UUID productId, @RequestBody ProductDto.Modify productDto) {
+        return productService.modifyProduct(productId, productDto);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ProductDto.DeleteResponse deleteProduct(@PathVariable UUID productId, @RequestBody ProductDto.Delete productDto) {
+        return productService.deleteProduct(productId, productDto);
+    }
+
+
+
+}
