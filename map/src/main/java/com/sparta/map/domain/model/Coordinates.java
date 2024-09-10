332bbcd344732f4b10b734b03b999574555c8b8a
leemiyeon <lemingul7@gmail.com>
2024-09-10 16:24:05 +0900

    feat: map 엔티티 구현

---
+++ map/src/main/java/com/sparta/map/domain/model/Coordinates.java
@@ -0,0 +1,11 @@
+package com.sparta.map.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinates {
+    private final double latitude;
+    private final double longitude;
+}
