0e37274b52d80d46499a87c3c1febf18985bc3f2
sanghun <tkdgns0236@gmail.com>
2024-09-11 14:49:14 +0900

    Feat: Company, Order, Product App Swagger UI 연동
    의존성 추가 및 Swagger UI 연동
    + Chrome에서 들어가지지 않는 버그로 인해 Company App의 포트 번호 수정 1 -> 88

--- order/src/main/java/com/sparta/order/controller/OrderController.java
+++ order/src/main/java/com/sparta/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.sparta.order.controller;
 
 import com.sparta.order.domain.dto.OrderDto;
 import com.sparta.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,30 +18,38 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @Operation(summary = "주문 생성", description = "주문을 생성합니다.")
     public OrderDto.Response createOrder(@RequestBody OrderDto.Response orderDto) {
         return orderService.createOrder(orderDto);
     }
 
     @GetMapping("/{orderId}")
+    @Operation(summary = "주문 단건 조회", description = "주문을 단건 조회합니다.")
     public OrderDto.Response getOrder(@PathVariable(value = "orderId") UUID orderId) {
         return orderService.getOrder(orderId);
     }
 
     @GetMapping
+    @Operation(summary = "주문 전체 조회", description = "주문을 페이징 조회합니다.")
     public List<OrderDto.GetAllOrdersResponse> getAllOrders(
+            @Schema(description = "페이지 번호(1부터 N까지)", defaultValue = "1")
             @RequestParam("page") int page,
+            @Schema(description = "페이지에 출력할 개수를 입력합니다.", defaultValue = "10")
             @RequestParam("size") int size,
+            @Schema(description = "정렬 기준을 입력합니다.")
             @RequestParam("sort") String sortBy
     ) {
         return orderService.getAllOrders(page, size, sortBy);
     }
 
     @PatchMapping("/{orderId}")
+    @Operation(summary = "주문 수정", description = "주문을 수정합니다.")
     public OrderDto.Response modifyOrder(@PathVariable(value = "orderId") UUID orderId, @RequestBody OrderDto.Modify orderDto) {
         return orderService.modifyOrder(orderId, orderDto);
     }
 
     @DeleteMapping("/{orderId}")
+    @Operation(summary = "주문 삭제", description = "주문을 삭제합니다.")
     public OrderDto.DeleteResponse deleteOrder(@PathVariable(value = "orderId") UUID orderId, @RequestBody OrderDto.Delete orderDto) {
         return orderService.deleteOrder(orderId, orderDto);
     }
