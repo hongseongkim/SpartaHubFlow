07fbc44209f4ef3f481194420d5012aa23674f41
leemiyeon <lemingul7@gmail.com>
2024-09-15 07:50:25 +0900

    refactor: [hub] hub-route repository refactoring

--- hub/src/main/java/com/sparta/hub/infrastructure/persistence/HubRouteRepository.java
+++ hub/src/main/java/com/sparta/hub/infrastructure/persistence/HubRouteRepository.java
@@ -4,22 +4,19 @@ import com.sparta.hub.domain.route.model.HubRoute;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HubRouteRepository extends JpaRepository<HubRoute, UUID> {
+    Optional<HubRoute> findByHubRouteIdAndIsDeletedFalse(UUID hubRouteId);
     Optional<HubRoute> findByOriginHubIdAndDestinationHubIdAndIsDeletedFalse(UUID originHubId, UUID destinationHubId);
 
-    List<HubRoute> findAllByIsDeletedFalse();
+    Optional<List<HubRoute>> findByOriginHubIdAndIsDeletedFalse(UUID originHubId);
+    Optional<List<HubRoute>> findByDestinationHubIdAndIsDeletedFalse(UUID destinationHubId);
 
-    List<HubRoute> findByOriginHubIdAndIsDeletedFalse(UUID originHubId);
-
-    List<HubRoute> findByDestinationHubIdAndIsDeletedFalse(UUID destinationHubId);
-
-    boolean existsByOriginHubIdAndDestinationHubIdAndIsDeletedFalse(UUID originHubId, UUID destinationHubId);
+    Page<HubRoute> findAllByIsDeletedFalse(Pageable pageable);
 
     // 특정 예상 소요 시간 이하의 경로 찾기
     List<HubRoute> findByEstimatedTimeLessThanEqualAndIsDeletedFalse(Integer estimatedTime);
-
-    // 특정 허브에서 출발하거나 도착하는 모든 경로 찾기
-    List<HubRoute> findByOriginHubIdOrDestinationHubIdAndIsDeletedFalse(UUID hubId, UUID sameHubId);
 }
