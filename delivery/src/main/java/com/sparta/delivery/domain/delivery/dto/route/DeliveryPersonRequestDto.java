ec68e2a7139f4595cd1788178a8ff73191a8cb4d
leemiyeon <lemingul7@gmail.com>
2024-09-17 12:56:30 +0900

    refactor: [delivery] delivery person logic refactoring - delivery person entity 추가

--- delivery/src/main/java/com/sparta/delivery/domain/delivery/dto/route/DeliveryPersonRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.delivery.domain.delivery.dto.route;
-
-import lombok.Getter;
-
-@Getter
-public class DeliveryPersonRequestDto {
-    private Long deliveryPersonId;
-    private String deliveryPersonSlackId;
-}
