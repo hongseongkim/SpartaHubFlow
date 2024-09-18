a18bf9b6b25479761b9d181755a10ec10febc652
kimongseong <rlaghdtjd123@gmail.com>
2024-09-18 14:40:14 +0900

    fix : 슬랙 yml 포트번호 , 컨트롤러url ,  security authorizeExchange 변경

--- slack/src/main/java/com/sparta/hotsix/slack/SlackController.java
+++ slack/src/main/java/com/sparta/hotsix/slack/SlackController.java
@@ -3,9 +3,11 @@ package com.sparta.hotsix.slack;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/slack")
 public class SlackController {
 
     private final SlackService slackService;
@@ -14,7 +16,7 @@ public class SlackController {
         this.slackService = slackService;
     }
 
-    @PostMapping("/slack/send")
+    @PostMapping("/send")
     public void sendSlackMessage(@RequestBody SlackMessageRequestDto requestDto) {
         slackService.sendMessage(requestDto);
     }
