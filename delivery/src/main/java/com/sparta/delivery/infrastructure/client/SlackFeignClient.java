519b22670c5a3103b3268f264fbc71f7c2c3d878
이미연 <120197266+leemiyeonn@users.noreply.github.com>
2024-09-19 14:50:51 +0900

    Update SlackFeignClient.java

--- delivery/src/main/java/com/sparta/delivery/infrastructure/client/SlackFeignClient.java
+++ delivery/src/main/java/com/sparta/delivery/infrastructure/client/SlackFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "slack-service")
+@FeignClient(name = "slack-service", configuration = FeignConfig.class)
 public interface SlackFeignClient {
     @PostMapping("/api/v1/slack")
     void sendSlackMessage(@RequestBody SlackMessageRequestDto requestDto);
